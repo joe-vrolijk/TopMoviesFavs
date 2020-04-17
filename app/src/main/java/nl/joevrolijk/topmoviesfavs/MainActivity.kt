9c3a970ac7fd902ee6044b9de0fda020eb2fb0d9
Joe Vrolijk <joe.vrolijk@gmail.com>
2020-04-17 19:57:28 +0200

    initial commit, lets get crackin

---
+++ app/src/main/java/nl/joevrolijk/topmoviesfavs/MainActivity.kt
@@ -0,0 +1,12 @@
+package nl.joevrolijk.topmoviesfavs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
