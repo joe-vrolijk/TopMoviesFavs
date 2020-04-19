165900e9e3bdd1ed184bcbb14db45acd733cd23a
Joe Vrolijk <joe.vrolijk@gmail.com>
2020-04-19 22:50:39 +0200

    Added RecyclerAdapter for Search movie results

---
+++ app/src/main/java/nl/joevrolijk/topmoviesfavs/ui/SearchMovieAdapter.kt
@@ -0,0 +1,45 @@
+package nl.joevrolijk.topmoviesfavs.ui
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_search_movie.view.*
+import nl.joevrolijk.topmoviesfavs.R
+import nl.joevrolijk.topmoviesfavs.model.Movie
+
+class SearchMovieAdapter(private val movies: List<Movie>, private val onClick: (Movie) -> Unit) :
+    RecyclerView.Adapter<SearchMovieAdapter.ViewHolder>() {
+
+    private lateinit var context: Context
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
+
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.item_search_movie, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: SearchMovieAdapter.ViewHolder, position: Int) =
+        holder.bind(movies[position])
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener {
+                onClick(movies[adapterPosition])
+            }
+        }
+
+        fun bind(movie: Movie) {
+            itemView.search_item_title.text = movie.title
+            itemView.search_item_plot.text = movie.overview
+            itemView.search_item_release.text = movie.releaseDate
+            Glide.with(context).load(movie.getPosterImageUrl()).into(itemView.search_item_poster)
+        }
+    }
+}
